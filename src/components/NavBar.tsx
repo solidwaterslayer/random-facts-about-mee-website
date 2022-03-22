12c44605b4a45310352aff495fb7bc67ba3b866f
Michael Tran <solidwater969@gmail.com>
2022-03-22 16:49:02 -0400

    refactor: update css in navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,24 @@
 import {Container, Nav, Navbar} from 'react-bootstrap'
-import {Link} from 'react-router-dom'
+import {Link, useLocation} from 'react-router-dom'
+import '../index.css'
 
-export default function NavBar() { return (
-	<Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
-		<Container>
-	    <Navbar.Brand className='text-danger'>useLocation().pathname</Navbar.Brand>
-			<Navbar.Toggle aria-controls='responsive-navbar-nav' />
-			<Navbar.Collapse id='responsive-navbar-nav'>
-				<Nav className='me-auto' />
-				<Nav>
-					<Nav.Link><Link to='/' className='text-light text-decoration-none'>home</Link></Nav.Link>
-					<Nav.Link><Link to='projects' className='text-light text-decoration-none'>projects</Link></Nav.Link>
-					<Nav.Link><Link to='about' className='text-light text-decoration-none'>about</Link></Nav.Link>
-				</Nav>
-			</Navbar.Collapse>
-		</Container>
-	</Navbar>
-)}
+export default function NavBar() {
+	return (
+		<Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+			<Container>
+				<Navbar.Brand className='text-danger'>{useLocation().pathname}</Navbar.Brand>
+				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+				<Navbar.Collapse id='responsive-navbar-nav'>
+					<Nav className='me-auto' />
+					<Nav>
+						<Nav.Link><Link to='/' className='nav-link'>home</Link></Nav.Link>
+						<Nav.Link><Link to='projects' className='nav-link'>projects</Link></Nav.Link>
+						<Nav.Link><Link to='about' className='nav-link'>about</Link></Nav.Link>
+					</Nav>
+					<Navbar.Brand className='text-danger'>L</Navbar.Brand>
+					<Navbar.Brand className='text-danger'>G</Navbar.Brand>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	)
+}
