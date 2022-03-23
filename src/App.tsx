272bd7c35649d85020ef22edd1de9634d6496d76
Michael Tran <solidwater969@gmail.com>
2022-03-22 20:56:50 -0400

    refactor: remove code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import { useEffect, useState } from 'react'
-import { Route, Routes, useLocation } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import About from './components/About'
 import Home from './components/Home'
 import NavBar from './components/NavBar'
 import Projects from './components/Projects'
 
 export default function App() {
-  let location = useLocation().pathname
-  if (!['/projects', '/about'].includes(location)) { location = '/' }
-
-  let [url, setURL] = useState(location)
-
-  useEffect(() => {
-    if (!['/projects', '/about'].includes(location)) { location = '/' }
-    setURL(location)
-  }, [location])
-
   return (
     <div className='bg-dark' style={{ fontFamily: 'Roboto Mono, monospace', fontSize: '1.1em', height: '100vh', }}>
-      <NavBar url={ url } setURL={ setURL }/>
+      <NavBar/>
       <Routes>
         <Route path='projects' element={ <Projects /> } />
         <Route path='about' element={ <About /> } />
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,26 +1,24 @@
+import { useEffect, useState } from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import '../index.css'
 
-export default function NavBar({ url, setURL }: any) {
-	function changeURL(event: any) {
-		let location = event.target.textContent
-		if (location === 'home') { location = '' }
-
-		setURL('/' + location)
-	}
+export default function NavBar() {
+	let location = useLocation().pathname
+  let [url, setURL] = useState(location)
+  useEffect(() => { setURL(location) }, [location])
 
 	return (
 		<Navbar bg='dark' collapseOnSelect expand='lg' variant='dark'>
 			<Container>
-				<Navbar.Brand className='text-danger'>{url}</Navbar.Brand>
+				<Navbar.Brand className='text-danger'>{ ['/projects', '/about'].includes(url) ? url : '/' }</Navbar.Brand>
 				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
 				<Navbar.Collapse id='responsive-navbar-nav'>
 					<Nav className='me-auto' />
 					<Nav>
-						<Nav.Link as={Link} className={ url === '/' ? 'nav-link active' : 'nav-link' } to='' onClick={ changeURL }>home</Nav.Link>
-						<Nav.Link as={Link} className={ url === '/projects' ? 'nav-link active' : 'nav-link' } to='projects' onClick={ changeURL }>projects</Nav.Link>
-						<Nav.Link as={Link} className={ url === '/about' ? 'nav-link active' : 'nav-link' } to='about' onClick={ changeURL }>about</Nav.Link>
+						<Nav.Link as={Link} className={ !['/projects', '/about'].includes(url) ? 'nav-link active' : 'nav-link' } to=''>home</Nav.Link>
+						<Nav.Link as={Link} className={ url === '/projects' ? 'nav-link active' : 'nav-link' } to='projects'>projects</Nav.Link>
+						<Nav.Link as={Link} className={ url === '/about' ? 'nav-link active' : 'nav-link' } to='about'>about</Nav.Link>
 					</Nav>
 					<Navbar.Brand className='text-danger'>L</Navbar.Brand>
 					<Navbar.Brand className='text-danger'>G</Navbar.Brand>
