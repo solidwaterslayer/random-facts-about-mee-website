456241d06d71f5062b36aa9824a226afa9c38b66
Michael Tran <solidwater969@gmail.com>
2022-03-24 16:54:01 -0400

    feature: add snowflake

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,25 @@
+import { faSnowflake } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import gsap from 'gsap'
+import { useEffect, useRef } from 'react'
 import Fact from './Fact'
 
 export default function Home() {
+	let snowflakeTriggerRef = useRef(null)
+	let snowflakeRef = useRef(null)
+
+  useEffect(() => {
+		let startLocation = Math.random() * 80 + 10
+		let endLocation = startLocation + Math.random() * 20 - 10
+		gsap.fromTo(snowflakeRef.current, { rotation: 0, x: startLocation + 'vw', y: '0vh', }, { duration: 3, ease: 'linear', repeat: -1, rotation: 360, scrollTrigger: { end: 'center 40%', markers: true, start: 'center 60%', trigger: snowflakeTriggerRef.current, toggleActions: 'play pause resume pause', }, x: endLocation + 'vw', y: '100vh', })
+		// gsap.fromTo(snowflakeRef.current, { scale: 0, x: '50vw', y: '50vh', }, { duration: 3, ease: 'linear', repeat: -1, scale: 100, x: '50vw', y: '50vh', })
+	})
+
 	return (
 		<>
+			<div className='position-fixed top-0 start-0 d-flex justify-content-center align-items-center' ref={ snowflakeRef } style={{ height: 0, width: 0, zIndex: -1, }}>
+				<FontAwesomeIcon className='text-light' icon={ faSnowflake } />
+			</div>
 			<Fact text='my least favorite number is 8 | its too symmetrical' />
 			<Fact text='my favorite animal is squid' />
 			<Fact text='i use light mode' />
@@ -12,7 +29,9 @@ export default function Home() {
 			<Fact text='i like nightcore' />
 			<Fact text='imm a minimalist' />
 			<Fact text='my favorite number is 9' />
-			<Fact text='my least favorite season is winter | when you are warm, you are unconfortable; but when you are cold, you are literally in pain' />
+			<div ref={ snowflakeTriggerRef }>
+				<Fact text='my least favorite season is winter | when you are warm, you are unconfortable; but when you are cold, you are literally in pain' />
+			</div>
 			<Fact text='imm ambidextrous' />
 			<Fact text='my favorite color is red' />
 		</>
