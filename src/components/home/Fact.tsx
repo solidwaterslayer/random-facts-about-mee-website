088da26f370c3ad6a7acc28d3fbb5b75a65fb936
Michael Tran <solidwater969@gmail.com>
2022-03-25 20:10:49 -0400

    refactor: add text shadow in fact

--- src/components/home/Fact.tsx
+++ src/components/home/Fact.tsx
@@ -1,5 +1,6 @@
 import gsap from 'gsap'
-import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import '../../index.css'
 import { useEffect, useRef } from 'react'
 
 export default function Fact({ customRef, text }: any) {
@@ -12,7 +13,7 @@ export default function Fact({ customRef, text }: any) {
   })
 
 	return (
-    <div className='my-5 py-5' ref={ factRef }>
+    <div className='my-5 py-5 hover' ref={ factRef }>
       <div className='my-5 py-5' ref={ customRef }>
         <h1 className={ Math.random() < .7 ? 'text-light mt-5 pt-5' : 'text-danger mt-5 pt-5' }>{ text.split(' | ')[0] }</h1>
         <p className='text-secondary mb-5 pb-5'>{ text.split(' | ')[1] }</p>
