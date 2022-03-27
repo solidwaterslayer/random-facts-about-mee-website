5e4921a076d84edfd01c01b870635234afbc24bc
Michael Tran <solidwater969@gmail.com>
2022-03-27 17:52:42 -0400

    feature: add attack

--- src/components/home/letters/Letters.tsx
+++ src/components/home/letters/Letters.tsx
@@ -19,15 +19,15 @@ export function Letters({ fightRef }: any) {
     gsap.from(downRef.current, { opacity: 0, scrollTrigger: { end: 'center 60%', scrub: true, start: 'center 110%', trigger: fightRef.current, }, })
     gsap.timeline({ repeat: -1, repeatDelay: 2, }).to(standingRef.current, { duration: '.1', y: '-1vh', }).to(standingRef.current, { ease: 'bounce', y: '0vh', })
     gsap.timeline({ delay: 2, scrollTrigger: { end: 'center -10%', start: 'center 110%', toggleActions: 'play reset play reset', trigger: fightRef.current, }, })
-    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '10vw', y: '-5vh', })
-    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '20vw', y: '-25vh', })
-    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '30vw', y: '-35vh', })
-    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '40vw', y: '-40vh', })
-    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '50vw', y: '-35vh', })
-    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '60vw', y: '-25vh', })
-    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '70vw', y: '-5vh', })
-    .to(jumpingRef.current, { duration: .1, ease: 'bounce', x: '80vw', y: '0vh', })
-    // gsap.timeline({}).from(attackRef.current, { opacity: 0, x: '80vw', y: '90vh', }).to(attackRef.current, { opacity: 1, scale: -1 }).to(attackRef.current, { opacity: 0 })
+    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '5vw', y: '-5vh', })
+    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '10vw', y: '-25vh', })
+    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '15vw', y: '-35vh', })
+    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '20vw', y: '-40vh', })
+    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '25vw', y: '-35vh', })
+    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '30vw', y: '-25vh', })
+    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '35vw', y: '-5vh', })
+    .to(jumpingRef.current, { duration: .1, ease: 'bounce', x: '40vw', y: '0vh', })
+    gsap.timeline({ delay: 2.7, scrollTrigger: { end: 'center -10%', start: 'center 110%', toggleActions: 'play reset play reset', trigger: fightRef.current, }, }).to(attackRef.current, { ease: 'power4', opacity: 1, }).to(attackRef.current, { opacity: 0, })
 	})
 
   return (
@@ -36,8 +36,8 @@ export function Letters({ fightRef }: any) {
         <div className='bottom-0 d-inline pe-none position-fixed text-danger' ref={ jumpingRef }>
           <h1 ref={ standingRef } style={{ lineHeight: .2, zIndex: -1 }}>n</h1>
         </div>
-        <div className='align-items-center d-flex justify-content-center pe-none position-fixed start-0 top-0' style={{ height: 0, width: 0, zIndex: -1, }}>
-          <img alt='' ref={ attackRef } src={ AttackTexture } />
+        <div className='align-items-center d-flex justify-content-center pe-none position-fixed' style={{ height: 0, left: '80vw', top: '90vh', width: 0, zIndex: -1, }}>
+          <img alt='' ref={ attackRef } src={ AttackTexture } style={{ opacity: 0, transform: 'scale(-1)', }}/>
         </div>
         <div className='bottom-0 d-inline pe-none position-fixed text-danger'>
           <h1 ref={ flyingRef } style={{ lineHeight: .2, zIndex: -1 }}>N</h1>
