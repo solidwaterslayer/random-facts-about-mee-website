a2158f2f18e9f5e7f41b18668ff3004dbc259ba2
Michael Tran <solidwater969@gmail.com>
2022-03-28 14:51:27 -0400

    refactor: extract methods in squid and letters

--- src/components/home/letters/Letters.tsx
+++ src/components/home/letters/Letters.tsx
@@ -4,10 +4,47 @@ import AttackTexture from './AttackTexture.gif'
 import ExplosionTexture from './ExplosionTexture.gif'
 import FireTexture from './FireTexture.gif'
 
+function fade(fightRef: any, upRef: any, downRef: any, jitteringRef: any, jumpingRef: any, attackRef: any, flyingRef: any, explosionRef: any, fireRef: any) {
+  gsap.to(upRef.current, { opacity: 0, scrollTrigger: { end: 'center -10%', scrub: true, start: 'center 40%', trigger: fightRef.current, }, })
+  gsap.from(downRef.current, { opacity: 0, scrollTrigger: { end: 'center 60%', scrub: true, start: 'center 110%', trigger: fightRef.current, }, })
+}
+
+function jitter(fightRef: any, upRef: any, downRef: any, jitteringRef: any, jumpingRef: any, attackRef: any, flyingRef: any, explosionRef: any, fireRef: any) {
+  gsap.timeline({ repeat: -1, repeatDelay: 2, }).to(jitteringRef.current, { duration: '.1', y: '-1vh', }).to(jitteringRef.current, { ease: 'bounce', y: '0vh', })
+}
+
+function jump(fightRef: any, upRef: any, downRef: any, jitteringRef: any, jumpingRef: any, attackRef: any, flyingRef: any, explosionRef: any, fireRef: any) {
+  gsap.timeline({ delay: 2, scrollTrigger: { end: 'center -10%', start: 'center 110%', toggleActions: 'play reset play reset', trigger: fightRef.current, }, })
+  .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '5vw', y: '-5vh', })
+  .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '10vw', y: '-25vh', })
+  .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '15vw', y: '-35vh', })
+  .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '20vw', y: '-40vh', })
+  .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '25vw', y: '-35vh', })
+  .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '30vw', y: '-25vh', })
+  .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '35vw', y: '-5vh', })
+  .to(jumpingRef.current, { duration: .1, ease: 'bounce', x: '40vw', y: '0vh', })
+}
+
+function attack(fightRef: any, upRef: any, downRef: any, jitteringRef: any, jumpingRef: any, attackRef: any, flyingRef: any, explosionRef: any, fireRef: any) {
+  gsap.timeline({ delay: 2.7, scrollTrigger: { end: 'center -10%', start: 'center 110%', toggleActions: 'play reset play reset', trigger: fightRef.current, }, }).to(attackRef.current, { ease: 'power4', opacity: 1, }).to(attackRef.current, { opacity: 0, })
+}
+
+function fly(fightRef: any, upRef: any, downRef: any, jitteringRef: any, jumpingRef: any, attackRef: any, flyingRef: any, explosionRef: any, fireRef: any) {
+  gsap.fromTo(flyingRef.current, { x: '80vw', }, { delay: 3, duration: .1, scrollTrigger: { end: 'center -10%', start: 'center 110%', toggleActions: 'play reset play reset', trigger: fightRef.current, }, x: '20vw', y: '-100vh', })
+}
+
+function explode(fightRef: any, upRef: any, downRef: any, jitteringRef: any, jumpingRef: any, attackRef: any, flyingRef: any, explosionRef: any, fireRef: any) {
+  gsap.timeline({ delay: 3.2, scrollTrigger: { end: 'center -10%', start: 'center 110%', toggleActions: 'play reset play reset', trigger: fightRef.current, }, }).to(explosionRef.current, { opacity: 1, }).to(explosionRef.current, { opacity: 0, })
+}
+
+function ignite(fightRef: any, upRef: any, downRef: any, jitteringRef: any, jumpingRef: any, attackRef: any, flyingRef: any, explosionRef: any, fireRef: any) {
+  gsap.to(fireRef.current, { delay: 3.5, opacity: 1, scrollTrigger: { end: 'center -10%', start: 'center 110%', toggleActions: 'play reset play reset', trigger: fightRef.current, }, })
+}
+
 export function Letters({ fightRef }: any) {
   let upRef = useRef(null)
   let downRef = useRef(null)
-  let standingRef = useRef(null)
+  let jitteringRef = useRef(null)
   let jumpingRef = useRef(null)
   let attackRef = useRef(null)
   let flyingRef = useRef(null)
@@ -15,36 +52,27 @@ export function Letters({ fightRef }: any) {
   let fireRef = useRef(null)
 
   useEffect(() => {
-    gsap.to(upRef.current, { opacity: 0, scrollTrigger: { end: 'center -10%', scrub: true, start: 'center 40%', trigger: fightRef.current, }, })
-    gsap.from(downRef.current, { opacity: 0, scrollTrigger: { end: 'center 60%', scrub: true, start: 'center 110%', trigger: fightRef.current, }, })
-    gsap.timeline({ repeat: -1, repeatDelay: 2, }).to(standingRef.current, { duration: '.1', y: '-1vh', }).to(standingRef.current, { ease: 'bounce', y: '0vh', })
-    gsap.timeline({ delay: 2, scrollTrigger: { end: 'center -10%', start: 'center 110%', toggleActions: 'play reset play reset', trigger: fightRef.current, }, })
-    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '5vw', y: '-5vh', })
-    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '10vw', y: '-25vh', })
-    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '15vw', y: '-35vh', })
-    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '20vw', y: '-40vh', })
-    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '25vw', y: '-35vh', })
-    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '30vw', y: '-25vh', })
-    .to(jumpingRef.current, { duration: .1, ease: 'linear', x: '35vw', y: '-5vh', })
-    .to(jumpingRef.current, { duration: .1, ease: 'bounce', x: '40vw', y: '0vh', })
-    gsap.timeline({ delay: 2.7, scrollTrigger: { end: 'center -10%', start: 'center 110%', toggleActions: 'play reset play reset', trigger: fightRef.current, }, }).to(attackRef.current, { ease: 'power4', opacity: 1, }).to(attackRef.current, { opacity: 0, })
-    gsap.fromTo(flyingRef.current, { x: '80vw', }, { delay: 3, duration: .1, scrollTrigger: { end: 'center -10%', start: 'center 110%', toggleActions: 'play reset play reset', trigger: fightRef.current, }, x: '20vw', y: '-100vh', })
-    gsap.timeline({ delay: 3.2, scrollTrigger: { end: 'center -10%', start: 'center 110%', toggleActions: 'play reset play reset', trigger: fightRef.current, }, }).to(explosionRef.current, { opacity: 1, }).to(explosionRef.current, { opacity: 0, })
-    gsap.to(fireRef.current, { delay: 3.5, opacity: 1, scrollTrigger: { end: 'center -10%', start: 'center 110%', toggleActions: 'play reset play reset', trigger: fightRef.current, }, })
+    fade(fightRef, upRef, downRef, jitteringRef, jumpingRef, attackRef, flyingRef, explosionRef, fireRef)
+    jitter(fightRef, upRef, downRef, jitteringRef, jumpingRef, attackRef, flyingRef, explosionRef, fireRef)
+    jump(fightRef, upRef, downRef, jitteringRef, jumpingRef, attackRef, flyingRef, explosionRef, fireRef)
+    attack(fightRef, upRef, downRef, jitteringRef, jumpingRef, attackRef, flyingRef, explosionRef, fireRef)
+    fly(fightRef, upRef, downRef, jitteringRef, jumpingRef, attackRef, flyingRef, explosionRef, fireRef)
+    explode(fightRef, upRef, downRef, jitteringRef, jumpingRef, attackRef, flyingRef, explosionRef, fireRef)
+    ignite(fightRef, upRef, downRef, jitteringRef, jumpingRef, attackRef, flyingRef, explosionRef, fireRef)
 	})
 
   return (
     <div ref={ upRef }>
       <div ref={ downRef }>
-        <div className='bottom-0 d-inline pe-none position-fixed text-danger' ref={ jumpingRef }>
-          <h1 ref={ standingRef } style={{ lineHeight: .2, zIndex: -1 }}>n</h1>
-        </div>
+        <span className='bottom-0 pe-none position-fixed text-danger' ref={ jumpingRef }>
+          <h1 ref={ jitteringRef } style={{ lineHeight: .2, zIndex: -1 }}>n</h1>
+        </span>
         <div className='image' style={{ left: '80vw', top: '90vh', }}>
           <img alt='' ref={ attackRef } src={ AttackTexture } style={{ opacity: 0, transform: 'scale(-1)', }} />
         </div>
-        <div className='bottom-0 d-inline pe-none position-fixed start-0 text-danger'>
+        <span className='bottom-0 pe-none position-fixed start-0 text-danger'>
           <h1 ref={ flyingRef } style={{ lineHeight: .2, zIndex: -1 }}>N</h1>
-        </div>
+        </span>
         <div className='image' style={{ left: '20vw', top: '15vh', }}>
           <img alt='' ref={ explosionRef } src={ ExplosionTexture } style={{ opacity: 0, transform: 'scaleY(-1)', }} />
         </div>
--- src/components/home/squid/Squid.tsx
+++ src/components/home/squid/Squid.tsx
@@ -2,31 +2,47 @@ import gsap from 'gsap'
 import { useEffect, useRef } from 'react'
 import SquidTexture from './SquidTexture.webp'
 
+function fade(swimRef: any, zoomRef: any, upRef: any, downRef: any, swimmingRef: any, jitteringRef: any, lookingRef: any) {
+  gsap.to(upRef.current, { opacity: 0, scrollTrigger: { end: 'center -10%', scrub: true, start: 'center 40%', trigger: zoomRef.current, }, })
+  gsap.from(downRef.current, { opacity: 0, scrollTrigger: { end: 'center 60%', scrub: true, start: 'center 110%', trigger: swimRef.current, }, })
+}
+
+function swim(swimRef: any, zoomRef: any, upRef: any, downRef: any, swimmingRef: any, jitteringRef: any, lookingRef: any) {
+  gsap.fromTo(swimmingRef.current, { rotation: 45, }, { ease: 'linear', opacity: 0, scrollTrigger: { end: 'center -10%', scrub: true, start: 'center 110%', trigger: swimRef.current, }, x: '100vw', y: '-100vh', })
+}
+
+function jitter(swimRef: any, zoomRef: any, upRef: any, downRef: any, swimmingRef: any, jitteringRef: any, lookingRef: any) {
+  gsap.timeline({ defaults: { duration: 3, }, repeat: -1, })
+  .to(jitteringRef.current, { x: '1vw', y: '-1vh', })
+  .to(jitteringRef.current, { x: '2vw', y: '1vw', })
+  .to(jitteringRef.current, { x: '0vw', y: '0vh', })
+}
+
+function look(swimRef: any, zoomRef: any, upRef: any, downRef: any, swimmingRef: any, jitteringRef: any, lookingRef: any) {
+  gsap.to(lookingRef.current, { scale: 2, scrollTrigger: { end: 'center 60%', scrub: true, start: 'center 110%', trigger: zoomRef.current, }, y: '40vh', })
+}
+
 export function Squid({ swimRef, zoomRef }: any) {
   let upRef = useRef(null)
   let downRef = useRef(null)
   let swimmingRef = useRef(null)
-  let floatingRef = useRef(null)
+  let jitteringRef = useRef(null)
   let lookingRef = useRef(null)
 
   useEffect(() => {
-    gsap.to(upRef.current, { opacity: 0, scrollTrigger: { end: 'center -10%', scrub: true, start: 'center 40%', trigger: zoomRef.current, }, })
-    gsap.from(downRef.current, { opacity: 0, scrollTrigger: { end: 'center 60%', scrub: true, start: 'center 110%', trigger: swimRef.current, }, })
-    gsap.fromTo(swimmingRef.current, { rotation: 45, }, { ease: 'linear', opacity: 0, scrollTrigger: { end: 'center -10%', scrub: true, start: 'center 110%', trigger: swimRef.current, }, x: '100vw', y: '-100vh', })
-    gsap.timeline({ defaults: { duration: 3, }, repeat: -1, })
-    .to(floatingRef.current, { x: '1vw', y: '-1vh', })
-    .to(floatingRef.current, { x: '2vw', y: '1vw', })
-    .to(floatingRef.current, { x: '0vw', y: '0vh', })
-    gsap.to(lookingRef.current, { scale: 2, scrollTrigger: { end: 'center 60%', scrub: true, start: 'center 110%', trigger: zoomRef.current, }, y: '40vh', })
+    fade(swimRef, zoomRef, upRef, downRef, swimmingRef, jitteringRef, lookingRef)
+    swim(swimRef, zoomRef, upRef, downRef, swimmingRef, jitteringRef, lookingRef)
+    jitter(swimRef, zoomRef, upRef, downRef, swimmingRef, jitteringRef, lookingRef)
+    look(swimRef, zoomRef, upRef, downRef, swimmingRef, jitteringRef, lookingRef)
 	})
 
   return (
     <div ref={ upRef }>
       <div ref={ downRef }>
-        <div className='image start-0 top-100' ref={ floatingRef }>
+        <div className='image start-0 top-100'>
           <img alt='' ref={ swimmingRef } src={ SquidTexture } />
         </div>
-        <div className='image' ref={ floatingRef } style={{ left: '80vw', top: '50vh', }}>
+        <div className='image' ref={ jitteringRef } style={{ left: '80vw', top: '50vh', }}>
           <img alt='' ref={ lookingRef } src={ SquidTexture } />
         </div>
       </div>
