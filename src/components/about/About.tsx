1ef981add2ee9d2f805538d6d4e2b9be01fe6487
Michael Tran <solidwater969@gmail.com>
2022-03-29 14:31:42 -0400

    feature: add links and icons in about

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -8,20 +8,46 @@ export default function About() {
 	return (
 		<div className='d-flex flex-column justify-content-center' style={{ height: '90vh', }}>
 			<Cube />
-			<h2 className='hover text-light'><FontAwesomeIcon className='pe-3' icon={ faLinkedin } />LinkedIn</h2>
-			<a className='hover mb-5 pb-5 text-danger text-decoration-none' href='https://www.linkedin.com/in/michael-tran-012345679/' rel='noreferrer noopener' target='_blank'>
-				https://www.linkedin.com/in/michael-tran-012345679/
-				<FontAwesomeIcon className='ps-3' icon={ faUpRightFromSquare } />
-			</a>
 
-			<h2 className='hover text-light'><FontAwesomeIcon className='pe-3' icon={ faGithub } />GitHub</h2>
-			<a className='hover mb-5 pb-5 text-danger text-decoration-none' href='https://github.com/solidwaterslayer' rel='noreferrer noopener' target='_blank'>
-				https://github.com/solidwaterslayer
-				<FontAwesomeIcon className='ps-3' icon={ faUpRightFromSquare } />
-			</a>
+			<div className='hover-activator text-light'>
+				<h2 className='hover-target mt-5'>
+					<FontAwesomeIcon className='pe-3' icon={ faLinkedin } />
+					LinkedIn
+				</h2>
+			</div>
+			<div className='hover-activator'>
+				<div className='hover-target mb-5'>
+					<a className='hover-target text-danger text-decoration-none' href='https://www.linkedin.com/in/michael-tran-012345679/' rel='noreferrer noopener' target='_blank'>
+						https://www.linkedin.com/in/michael-tran-012345679/
+						<FontAwesomeIcon className='ps-3' icon={ faUpRightFromSquare } />
+					</a>
+				</div>
+			</div>
 
-			<h2 className='hover text-light'><FontAwesomeIcon className='pe-3' icon={ faMailBulk } />Email</h2>
-			<p className='hover mb-5 pb-5 text-danger'>solidwater969@gmail.com</p>
+			<div className='hover-activator text-light'>
+				<h2 className='hover-target mt-5'>
+					<FontAwesomeIcon className='pe-3' icon={ faGithub } />
+					GitHub
+				</h2>
+			</div>
+			<div className='hover-activator'>
+				<div className='hover-target mb-5'>
+					<a className='hover-target text-danger text-decoration-none' href='https://github.com/solidwaterslayer' rel='noreferrer noopener' target='_blank'>
+						https://github.com/solidwaterslayer
+						<FontAwesomeIcon className='ps-3' icon={ faUpRightFromSquare } />
+					</a>
+				</div>
+			</div>
+
+			<div className='hover-activator text-light'>
+				<h2 className='hover-target mt-5'>
+					<FontAwesomeIcon className='pe-3' icon={ faMailBulk } />
+					Email
+				</h2>
+			</div>
+			<div className='hover-activator text-danger'>
+				<p className='hover-target mb-5'>solidwater969@gmail.com</p>
+			</div>
 		</div>
 	)
 }
