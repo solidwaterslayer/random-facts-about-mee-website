86360f20ac237bd13f3d2d0a8ce4cbe8dea85398
Michael Tran <solidwater969@gmail.com>
2022-03-31 19:15:04 -0400

    fix: add plays inline

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -8,7 +8,7 @@ export default function Projects() {
 				<div className='banner bg-dark bg-gradient text-light'>
 					<div className='mx-0 row'>
 						<div className='col-md-6'></div>
-						<div className='col-md-6 col-sm-12'><video autoPlay className='w-100' loop muted src={ randomFactsAboutMeeWebsite } /></div>
+						<div className='col-md-6 col-sm-12'><video autoPlay className='w-100' loop muted playsInline src={ randomFactsAboutMeeWebsite } /></div>
 					</div>
 					<div className='mx-0 position-absolute row w-100'>
 						<div className='col-md-6 col-sm-12'><h1 className='banner-title'>[4th] Random Facts About Mee Website</h1></div>
@@ -18,7 +18,7 @@ export default function Projects() {
 
 				<div className='banner bg-gradient bg-light text-danger'>
 					<div className='mx-0 row'>
-						<div className='col-md-6 col-sm-12'><video autoPlay className='w-100' loop muted src={ randomFactsAboutMeeWebsite } /></div>
+						<div className='col-md-6 col-sm-12'><video autoPlay className='w-100' loop muted playsInline src={ randomFactsAboutMeeWebsite } /></div>
 						<div className='col-md-6'></div>
 					</div>
 					<div className='mx-0 position-absolute row w-100'>
@@ -30,7 +30,7 @@ export default function Projects() {
 				<div className='banner bg-gradient bg-danger text-light'>
 					<div className='mx-0 row'>
 						<div className='col-md-6'></div>
-						<div className='col-md-6 col-sm-12'><video autoPlay className='w-100' loop muted src={ randomFactsAboutMeeWebsite } /></div>
+						<div className='col-md-6 col-sm-12'><video autoPlay className='w-100' loop muted playsInline src={ randomFactsAboutMeeWebsite } /></div>
 					</div>
 					<div className='mx-0 position-absolute row w-100'>
 						<div className='col-md-6 col-sm-12'><h1 className='banner-title'>[2nd] Paper Pushing Game</h1></div>
@@ -40,7 +40,7 @@ export default function Projects() {
 
 				<div className='banner bg-gradient bg-dark text-light'>
 					<div className='mx-0 row'>
-						<div className='col-md-6 col-sm-12'><video autoPlay className='w-100' loop muted src={ randomFactsAboutMeeWebsite } /></div>
+						<div className='col-md-6 col-sm-12'><video autoPlay className='w-100' loop muted playsInline src={ randomFactsAboutMeeWebsite } /></div>
 						<div className='col-md-6'></div>
 					</div>
 					<div className='mx-0 position-absolute row w-100'>
@@ -52,7 +52,7 @@ export default function Projects() {
 				<div className='banner bg-gradient bg-secondary text-light'>
 					<div className='mx-0 row'>
 						<div className='col-md-6'></div>
-						<div className='col-md-6 col-sm-12'><video autoPlay className='w-100' loop muted src={ randomFactsAboutMeeWebsite } /></div>
+						<div className='col-md-6 col-sm-12'><video autoPlay className='w-100' loop muted playsInline src={ randomFactsAboutMeeWebsite } /></div>
 					</div>
 					<div className='mx-0 position-absolute row w-100'>
 						<div className='col-md-6 col-sm-12'><h1 className='banner-title'>[0th] Ball Game</h1></div>
