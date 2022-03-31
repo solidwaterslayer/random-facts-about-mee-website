70d7efea27202dda37e56f7b0e8f17001e00435d
Michael Tran <solidwater969@gmail.com>
2022-03-31 12:04:36 -0400

    feature: add banner

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -5,12 +5,19 @@ export default function Projects() {
 	return (
 		<>
 			<div>
-				<div className='banner bg-gradient bg-secondary'>
-					<video autoPlay loop muted src={ randomFactsAboutMeeWebsite } style={{ height: '400px', }} />
+				<div className='align-items-center banner bg-dark bg-gradient d-flex'>
+					<div className='mx-0 row'>
+						<div className='col-md-6'></div>
+						<div className='col-md-6 col-sm-12'><video autoPlay className='w-100' loop muted src={ randomFactsAboutMeeWebsite } /></div>
+					</div>
+					<div className='mx-0 position-absolute row text-light w-100'>
+						<div className='col-md-6 col-sm-12'><h1>Random Facts About Mee Website [This project lol.]</h1></div>
+						<div className='col-md-6'></div>
+					</div>
 				</div>
-				<div className='banner bg-dark bg-gradient'>imm currently working on this page</div>
-				<div className='banner bg-danger bg-gradient'>imm currently working on this page</div>
 				<div className='banner bg-gradient bg-light'>imm currently working on this page</div>
+				<div className='banner bg-danger bg-gradient'>imm currently working on this page</div>
+				<div className='banner bg-gradient bg-secondary'>imm currently working on this page</div>
 			</div>
 			<div className='text-danger'>imm currently working on this page</div>
 			<div className='text-danger'>imm currently working on this page</div>
