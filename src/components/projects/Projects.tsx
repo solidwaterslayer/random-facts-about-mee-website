d172006e68664464d64783d55c1a99bc474a3e0c
Michael Tran <solidwater969@gmail.com>
2022-03-31 20:46:46 -0400

    feature: add banner

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -9,6 +9,7 @@ export default function Projects() {
 	return (
 		<>
 			<div>
+				<div className='banner banner-title bg-gradient bg-secondary justify-content-center text-light'>Here's A Curated List Of My Previous Projects OwO</div>
 				<Banner theme='bg-dark text-light' isVideoLeft={ false } video={ randomFactsAboutMeeWebsite } title='[4th] Random Facts About Mee Website'></Banner>
 				<Banner theme='bg-light text-danger' isVideoLeft={ true } video={ diamondCollectingGame } title='[3rd] Diamond Collecting Game'></Banner>
 				<Banner theme='bg-danger text-light' isVideoLeft={ false } video={ paperPushingGame } title='[2nd] Paper Pushing Game'></Banner>
@@ -16,12 +17,28 @@ export default function Projects() {
 				<Banner theme='bg-secondary text-light' isVideoLeft={ false } video={ ballGame } title='[0th] Ball Game'></Banner>
 			</div>
 			<div className='text-danger'>imm currently working on this page</div>
-			<div className='text-danger'>make a banner component</div>
 			<div className='text-danger'>blend title and video together</div>
-			<div className='text-danger'>record video</div>
 			<div className='text-danger'>make accordian</div>
 			<div className='text-danger'>update readme</div>
 			<div className='text-danger'>update github and linkedin</div>
+			<div className='text-danger'>show everyone</div>
+			<div className='text-danger'>show everyone</div>
+			<div className='text-danger'>show everyone</div>
+			<div className='text-danger'>show everyone</div>
+			<div className='text-danger'>show everyone</div>
+			<div className='text-danger'>show everyone</div>
+			<div className='text-danger'>show everyone</div>
+			<div className='text-danger'>show everyone</div>
+			<div className='text-danger'>show everyone</div>
+			<div className='text-danger'>show everyone</div>
+			<div className='text-danger'>show everyone</div>
+			<div className='text-danger'>show everyone</div>
+			<div className='text-danger'>show everyone</div>
+			<div className='text-danger'>show everyone</div>
+			<div className='text-danger'>show everyone</div>
+			<div className='text-danger'>show everyone</div>
+			<div className='text-danger'>show everyone</div>
+			<div className='text-danger'>show everyone</div>
 			<div className='text-danger'>show everyone</div>
 		</>
 	)
